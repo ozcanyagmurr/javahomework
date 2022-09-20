9ccd611fab6017cab68b16482e7bb9364e0ed096
Yağmur Özcan <ozcanyagmur3513@gmail.com>
2022-09-20 16:48:35 +0300

    java hw

--- java1/src/java1/Video5.java
+++ java1/src/java1/Video5.java
@@ -3,7 +3,9 @@ package java1;
 public class Video5 {
 
 	public static void main(String[] args) {
-		System.out.println("Merhaba Java Video 5");
+		System.out.println("Merhaba Java Video 5!");
+		
+		
 		
 
 	}
---
+++ java1/src/java1/Video8.java
@@ -0,0 +1,17 @@
+package java1;
+
+public class Video8 {
+
+	public static void main(String[] args) {
+		int sayı = 24;
+		if(sayı<20) {
+			System.out.println("Sayı yirmiden küçüktür");
+		}else if(sayı==20) {
+			System.out.println("Sayı yirmiye eşittir");
+		}else {
+			System.out.println("Sayı yirmiden büyüktür");
+		}
+
+	}
+
+}
