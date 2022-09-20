f54beb9b9323a9703f55ab78f5d707052e744f0c
Yağmur Özcan <ozcanyagmur3513@gmail.com>
2022-09-20 17:11:24 +0300

    java hw

---
+++ java1/src/java1/Video9.java
@@ -0,0 +1,21 @@
+package java1;
+
+public class Video9 {
+
+	public static void main(String[] args) {
+		int sayi1 = 20;
+		int sayi2 = 25;
+		int sayi3 =2;
+		int enBüyük = sayi1;
+	 if (enBüyük<sayi2) {
+		 System.out.println("25 en büyüktür");
+		
+	 }else if(enBüyük<sayi3) {
+		 System.out.println("2 en büyüktür");
+		 
+	 }else
+		 System.out.println("20 en büyüktür");
+
+	}
+
+}
