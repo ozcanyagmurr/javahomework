f354e765ce01ea9152690e90a29ef684cbf595ee
Yağmur Özcan <ozcanyagmur3513@gmail.com>
2022-09-20 14:11:28 +0300

    video6

---
+++ java1/src/java1/Video6.java
@@ -0,0 +1,12 @@
+package java1;
+
+public class Video6 {
+
+	public static void main(String[] args) {
+		int ürün = 100 ;
+		String depo = "ürün :";
+		System.out.println(depo + ürün);
+		System.out.println("depodaki ürün : "+ ürün);
+	}
+
+}
