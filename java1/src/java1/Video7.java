b05a388c46f04901671c9bde816f1ed35855b2de
Yağmur Özcan <ozcanyagmur3513@gmail.com>
2022-09-20 15:54:50 +0300

    java hw

---
+++ java1/src/java1/Video7.java
@@ -0,0 +1,28 @@
+package java1;
+
+public class Video7 {
+
+	public static void main(String[] args) {
+		int sayi = 125864845 ;
+		sayi = 1235875 ;
+		
+		byte sayii = 15;
+		sayii = 127;
+		sayii = -128;
+		// byte -128 ve 127 arasındaki değerleri alır
+		
+		double ssayi = 142.56;
+		ssayi = 158.69;
+		//doble ondalıklı sayıları tutmak içindir
+		
+		char karakter = 'B';
+		// tek karakter tutmak istediğimiz zaman char veri tipinden yarlanırız
+		
+		boolean dogruMu = false;
+		
+		if (dogruMu==true) {
+			
+		}
+	}
+
+}
