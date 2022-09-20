09a9b36cf6d5972a937b1e9feb2152cd065b5a83
Yağmur Özcan <ozcanyagmur3513@gmail.com>
2022-09-20 19:48:18 +0300

    java hw

---
+++ java1/src/java1/Video13_DoWhileDöngüsü.java
@@ -0,0 +1,14 @@
+package java1;
+
+public class Video13_DoWhileDöngüsü {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println(i);
+			i+=2;
+		}while(i<10);
+		System.out.println("do while döngüsü bitti");
+	}
+
+}
---
+++ java1/src/java1/Video15_ReCapDemo2_DizilerleÇalışmak.java
@@ -0,0 +1,10 @@
+package java1;
+
+public class Video15_ReCapDemo2_DizilerleÇalışmak {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
