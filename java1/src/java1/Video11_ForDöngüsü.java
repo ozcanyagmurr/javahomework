1b262498c2cf9df46acad02b86db5c8b5702fe0b
Yağmur Özcan <ozcanyagmur3513@gmail.com>
2022-09-20 18:32:17 +0300

    java hw

---
+++ java1/src/java1/Video11_ForDöngüsü.java
@@ -0,0 +1,13 @@
+package java1;
+
+public class Video11_ForDöngüsü {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <10; i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("döngü bitti");
+	}
+}
+
