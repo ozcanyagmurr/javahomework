83212e48cd87368ed0e1725df4a4364d3d61bc20
Yağmur Özcan <ozcanyagmur3513@gmail.com>
2022-09-25 23:17:58 +0300

    java hw

---
+++ java2/src/java2/Video24_25_MetodlarıAnlamak.java
@@ -0,0 +1,26 @@
+package java2;
+
+public class Video24_25_MetodlarıAnlamak {
+
+	public static void main(String[] args) {
+		String mesaj ="Bugün hava çok güzel.";
+		String yeniMesaj = sehirVer();
+		System.out.println(yeniMesaj);
+		int sayi = topla(5,7);
+		System.out.println(sayi);
+		
+		
+	}
+	public static void ekle() {
+		System.out.println("Eklendi");
+	}
+	public static void sil() {
+		System.out.println("Silindi");
+	}
+	public static int topla(int sayi1, int sayi2) {
+		return sayi1+sayi2;
+	}
+	public static String sehirVer() {
+		return "Ankara";
+	}
+}
