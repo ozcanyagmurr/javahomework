3d0ee313d709173c4a8f498e67c8608a56b4d70a
Yağmur Özcan <ozcanyagmur3513@gmail.com>
2022-09-25 23:16:39 +0300

    java hw

---
+++ video30_reCapDemoClasses/src/Main.java
@@ -0,0 +1,11 @@
+import video30_reCapDemoClasses.Dortİslem;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Dortİslem dortİslem = new Dortİslem();
+		int sonuc= dortİslem.Topla(3, 4);
+		System.out.println(sonuc);
+	}
+
+}
---
+++ video30_reCapDemoClasses/src/video30_reCapDemoClasses/Dortİslem.java
@@ -0,0 +1,18 @@
+package video30_reCapDemoClasses;
+
+public class Dortİslem {
+	public int Topla(int sayi1, int sayi2) {
+		return sayi1+sayi2;
+	}
+	
+	public int Cikar(int sayi1, int sayi2 ) {
+		return sayi1-sayi2;
+		
+	}
+	public int Carp (int sayi1,int sayi2) {
+		return sayi1*sayi2;
+}
+	public int Bol(int sayi1, int sayi2) {
+		return sayi1/sayi2;
+	}
+}
