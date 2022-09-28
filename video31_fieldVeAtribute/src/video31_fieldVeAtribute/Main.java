3d775862a1baa58be3a882dcf626828f910df6ef
Yağmur Özcan <ozcanyagmur3513@gmail.com>
2022-09-28 13:40:41 +0300

    java hw

---
+++ video31_fieldVeAtribute/src/video31_fieldVeAtribute/Main.java
@@ -0,0 +1,17 @@
+package video31_fieldVeAtribute;
+
+public class Main {
+
+	public static void main(String[]args) {
+		Product product =new Product();
+		product.name="Laptop";
+		product.id=1;
+		product.description="Asus Laptop";
+		product.price=5000;
+		product.stockAmount=3;
+		
+		
+		ProductManager productManager=new ProductManager();
+		productManager.Add(product);
+	}
+}
---
+++ video31_fieldVeAtribute/src/video31_fieldVeAtribute/ProductManager.java
@@ -0,0 +1,9 @@
+package video31_fieldVeAtribute;
+
+public class ProductManager {
+	public void Add(Product product) {
+		//JDBC
+		System.out.println("Ürün eklendi" + product.name);
+	}
+
+}
